23df89d797ac915a2e25dbc6df1d1870090643d3
atulrwt-fs <215132650+atulrwt-fs@users.noreply.github.com>
2026-01-22 23:25:36 -0800

    Atul | Removed unnecessary dependencies

--- src/main/java/my/netflix/clone/service/UserService.java
+++ src/main/java/my/netflix/clone/service/UserService.java
@@ -2,6 +2,7 @@ package my.netflix.clone.service;
 
 import my.netflix.clone.entity.User;
 import my.netflix.clone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +10,12 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User createUser() {
         User testUser = new User();
